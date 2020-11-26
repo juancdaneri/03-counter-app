f22c95af0a1d8f7b41b8dd088bd0573c931875eb
Juan <juanjcd97@gmail.com>
2020-11-26 16:50:26 -0300

    Final sección 4

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,17 +1,35 @@
 import Proptypes from 'prop-types';
+import React, { useState } from 'react'
+//los USExxxx son hooks
+
+
+const CounterApp = ({value = 10}) => {
+
+    const [counter , setCounter] = useState(value);
 
 
 
-const CounterApp = ({value}) => {
-
-    const handleAdd = (e) => {
-        console.log(e)
+    const handleAdd = () => {
+        setCounter( counter + 1 );
+        //setCounter( (c) => c + 1)
     }
+
+    
+    const handleSubstract = () => setCounter(counter - 1);
+    
+
+    const handleReset = () => setCounter(value);
+    
+    
+
+
 
     return(
         <>
         <h1>CounterApp</h1>
-        <h2>{value}</h2>
+        <h2>{ counter }</h2>
+        <button onClick={handleSubstract}>-1</button>
+        <button onClick={handleReset}>Reset</button>
         <button onClick={handleAdd}>+1</button>
         </>
     )
